b7cb1b2ea13480694d995ce7e17ae5a893d1cb0f
Nicole Tan <geniusnic009@gmail.com>
2023-11-20 10:27:13 +0800

    finished add trip page

--- travel app/NewTripsView.swift
+++ travel app/NewTripsView.swift
@@ -12,25 +12,12 @@ struct NewTripsView: View {
     @State private var name = ""
     @State private var startDate = Date()
     @State private var endDate = Date()
+    @State private var flight = ""
     
     var body: some View {
         VStack {
         NavigationView {
-            Text(".")
-                .navigationTitle("Navigation Title")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing){
-                        Button("Add") {
-                            print("pressed")
-                        }
-                    }
-                    ToolbarItem(placement: .navigationBarLeading){
-                        Button("Cancel") {
-                            print("Pressed")
-                        }
-                    }
-                }
-        }
+         //  Text(".")
             Form {
                 Section("Name") {
                     TextField("Enter Trip Name", text: $name)
@@ -48,9 +35,51 @@ struct NewTripsView: View {
                                 displayedComponents: [.date]
                     )
                 }
+                
+                Section("Flight") {
+                    TextField("Enter Flight Number", text: $flight)
+                }
 
                 
             }
+                .navigationTitle("Navigation Title")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        Button("Add") {
+                            print("pressed")
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarLeading){
+                        Button("Cancel") {
+                            print("Pressed")
+                        }
+                    }
+                }
+        }
+//            Form {
+//                Section("Name") {
+//                    TextField("Enter Trip Name", text: $name)
+//                }
+//
+//                Section("Date") {
+//                    DatePicker(
+//                        "Start Date",
+//                                selection: $startDate,
+//                                displayedComponents: [.date]
+//                    )
+//                    DatePicker(
+//                        "End Date",
+//                                selection: $endDate,
+//                                displayedComponents: [.date]
+//                    )
+//                }
+//
+//                Section("Flight") {
+//                    TextField("Enter Flight Number", text: $flight)
+//                }
+//
+//
+//            }
 
             
             
