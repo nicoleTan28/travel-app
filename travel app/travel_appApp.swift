d9ebc4b6f3740a4b10183dbb053070ff214995c5
Nicole Tan <geniusnic009@gmail.com>
2023-11-20 09:25:41 +0800

    Initial Commit

---
+++ travel app/travel_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  travel_appApp.swift
+//  travel app
+//
+//  Created by T Krobot on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct travel_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
