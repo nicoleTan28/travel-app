b9bc5b4d61e18ec0d4c9b25cae20d8b0499c5d44
Nicole Tan <geniusnic009@gmail.com>
2023-11-24 09:07:53 +0800

    .

--- travel app/Trips/Itinerary/SearchView.swift
+++ travel app/Trips/Itinerary/SearchView.swift
@@ -8,23 +8,21 @@
 import SwiftUI
 
 struct SearchView: View {
-    let names = ["Holly", "Josh", "Rhonda", "Ted"]
+    let names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17"]
     @State private var searchText = ""
-
+    
     var body: some View {
         NavigationStack {
             List {
                 ForEach(searchResults, id: \.self) { name in
-                     
-                        Text(name)
-                    
+                    Text(name)
                 }
             }
             .navigationTitle("Contacts")
         }
         .searchable(text: $searchText)
     }
-
+    
     var searchResults: [String] {
         if searchText.isEmpty {
             return names
