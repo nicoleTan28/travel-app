57327301a96a7fd3c9b9d644881331d6894266ae
Danin <chhindanin@gmail.com>
2023-11-24 15:25:50 +0800

    added sample json file, edited list of location names

--- travel app/Trips/Itinerary/LocationStruct.swift
+++ travel app/Trips/Itinerary/LocationStruct.swift
@@ -13,3 +13,29 @@ struct Location: Identifiable, Codable {
     var startTime = Date()
     var endTime = Date()
 }
+
+//Danin's json decoding stuff? is it??
+func loadJson(filename fileName: String) -> [attract]? {
+    if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            let jsonData = try decoder.decode(ResponseData1.self, from: data)
+            return jsonData.features
+            
+        } catch {
+            print("error:\(error)")
+        }
+    }
+    return nil
+}
+
+struct ResponseData1: Decodable{
+    var features: [attract]
+}
+
+struct attract: Decodable,Hashable{
+    var name: String
+    var overview: String
+   var attractImage: String?
+}
--- travel app/Trips/Itinerary/SearchView.swift
+++ travel app/Trips/Itinerary/SearchView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct SearchView: View {
-    
-    let names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "A", "D", "C", "M", "X"]
+    let fileName = "sample3"
+    @State var names: [String] = []
     @State private var searchText = ""
     @Environment(\.dismiss) var dismiss
     @Binding var locationSource: [Location]
@@ -19,20 +19,29 @@ struct SearchView: View {
     
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(names.sorted(), id: \.self) { name in
-                    Text(name)
-                        .onTapGesture {
-                            if let index = names.firstIndex(of: name) { //finds index of tapped name
-                                selectedIndex = index //sets selectedIndex to said index
-                                locationSource.append(Location(name: name)) //adding to a 'list'
-                                dismiss()
-                                valueFromSheet = name //tapped name
+            VStack {
+                List {
+                    ForEach(names.sorted(), id: \.self) { name in
+                        Text(name)
+                            .onTapGesture {
+                                if let index = names.firstIndex(of: name) { //finds index of tapped name
+                                    selectedIndex = index //sets selectedIndex to said index
+                                    locationSource.append(Location(name: name)) //adding to a 'list'
+                                    dismiss()
+                                    valueFromSheet = name //tapped name
+                                }
                             }
-                        }
+                    }
+                }
+                .navigationTitle("Locations")
+            }
+            .onAppear{
+                if let places = loadJson(filename: fileName) {
+                    for place in places {
+                        names.append(place.name)
+                    }
                 }
             }
-            .navigationTitle("Locations")
         }
         .searchable(text: $searchText)
     }
