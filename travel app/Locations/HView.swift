a46e1c579ea84fea6c3ffeb6ab5ea332eae0b88e
akshaj321 <135086913+akshaj321@users.noreply.github.com>
2023-11-26 22:46:08 +0800

    aaaa

---
+++ travel app/Locations/HView.swift
@@ -0,0 +1,20 @@
+//
+//  HView.swift
+//  travel app
+//
+//  Created by Shakthi Vel on 26/11/23.
+//
+
+import SwiftUI
+
+struct HView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HView()
+}
