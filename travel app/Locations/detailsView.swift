0e42f93697abffea41c703313a07fdf9a4f25a7f
akshaj321 <135086913+akshaj321@users.noreply.github.com>
2023-11-27 11:04:31 +0800

    ...

---
+++ travel app/Locations/detailsView.swift
@@ -0,0 +1,18 @@
+//
+//  detailsView.swift
+//  travel app
+//
+//  Created by Shakthi Vel on 27/11/23.
+//
+
+import SwiftUI
+
+struct detailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    detailsView()
+}
