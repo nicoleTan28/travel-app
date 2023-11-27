204315e73832aeb75cfde3aec7f35b21a7560303
akshaj321 <135086913+akshaj321@users.noreply.github.com>
2023-11-27 14:21:51 +0800

    More merge fixes

--- travel app/Locations/detailsView.swift
+++ travel app/Locations/detailsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct detailsView: View {
 <<<<<<< HEAD
+<<<<<<< HEAD
     var place: attract
     
     var body: some View {
@@ -27,12 +28,26 @@ struct detailsView: View {
 }
 
 =======
+=======
+    var place: attract
+    
+>>>>>>> cd565b0 (...)
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(place.name)
+                .font(.title)
+            Text(place.overview)
+                .padding()
+            
+        }
+        .navigationTitle("Details")
     }
 }
-
 #Preview {
-    detailsView()
+    detailsView(place: attract(name: "Sample",overview: "This is a sample overview"))// attractImage: "1095-national-gallery-singapore-carousel-01-rec"))
 }
+<<<<<<< HEAD
 >>>>>>> 0e42f93 (...)
+=======
+
+>>>>>>> cd565b0 (...)
